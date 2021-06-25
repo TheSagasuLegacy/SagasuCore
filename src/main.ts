90cd12780160c37431ea38f742cbebf77e1c418b
Mix <32300164+mnixry@users.noreply.github.com>
2021-06-25 20:59:06 +0800

    :art: Add Swagger API document support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle(process.env.npm_package_name)
+    .setDescription(process.env.npm_package_description)
+    .setVersion(process.env.npm_package_version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
