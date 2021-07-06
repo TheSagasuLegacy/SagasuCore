36e475bb3ef0639618a34426a3714250d848b983
Mix <32300164+mnixry@users.noreply.github.com>
2021-07-07 00:23:07 +0800

    :wrench: Make `REDIS_URL` able to configure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { BullModule } from '@nestjs/bull';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -12,8 +12,20 @@ import { SeriesModule } from './series/series.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(),
-    BullModule.forRoot({}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        url: config.get<string>('DATABASE_URL'),
+      }),
+    }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        redis: config.get<string>('REDIS_URL'),
+      }),
+    }),
     SeriesModule,
     ScraperModule,
     DialogsModule,
