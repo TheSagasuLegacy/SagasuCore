6823e36eea630437f38b2ac644f40e0cd62e3c4b
Mix <32300164+mnixry@users.noreply.github.com>
2021-07-07 23:56:55 +0800

    :bug: Fix environment variable `DATABASE_URL` cannot be loaded

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const dotenv = require('dotenv');
 
+dotenv.config();
+
 module.exports = {
   type: 'postgres',
-  url: dotenv.config().parsed?.DATABASE_URL,
+  url: process.env.DATABASE_URL,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migration/*{.ts,.js}'],
   cli: {
