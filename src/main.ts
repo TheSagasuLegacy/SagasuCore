2ac340cd79df2c2e67ad32bb1470e2226994a6da
Mix <32300164+mnixry@users.noreply.github.com>
2021-07-11 18:39:48 +0800

    :bug: Remove `ValidationPipe` to fix empty request error

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -6,7 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
   const config = new DocumentBuilder()
     .setTitle(process.env.npm_package_name)
