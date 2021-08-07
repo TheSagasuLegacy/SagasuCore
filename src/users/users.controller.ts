9ec96b0234dc6672d26035ed3714ae417b6ff3ba
Mix <32300164+mnixry@users.noreply.github.com>
2021-08-08 00:49:24 +0800

    :sparkles: Finish basic of entity `user` crud operation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,44 +1,17 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Crud, CrudController } from '@nestjsx/crud';
+import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
+@Crud({
+  model: { type: User },
+  routes: { exclude: ['replaceOneBase'] },
+  params: { name: { field: 'name', type: 'string', primary: true } },
+  query: { alwaysPaginate: true, maxLimit: 50 },
+})
 @ApiTags('users')
 @Controller('users')
-export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
-  }
+export class UsersController implements CrudController<User> {
+  constructor(public service: UsersService) {}
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRoles } from './entities/user-roles.entity';
+import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, UserRoles])],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [TypeOrmModule],
 })
 export class UsersModule {}
