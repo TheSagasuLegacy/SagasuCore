305732a61dabedc6c22a1e1776b9376593a286d0
Mix <32300164+mnixry@users.noreply.github.com>
2021-08-09 00:51:32 +0800

    :art: Extract subscribers method in user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,17 +1,12 @@
-import * as argon2 from 'argon2';
 import { Exclude } from 'class-transformer';
 import {
-  BeforeInsert,
-  BeforeUpdate,
   Column,
   CreateDateColumn,
   Entity,
   Index,
-  InsertEvent,
   OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
-  UpdateEvent,
 } from 'typeorm';
 import { UserRoles } from './user-roles.entity';
 
@@ -31,28 +26,6 @@ export class User {
   @Exclude()
   @Column()
   password: string;
-
-  @BeforeInsert()
-  async passwordInsert(event: InsertEvent<User>) {
-    if (event.entity.password) {
-      event.entity.password = await argon2.hash(event.entity.password);
-    }
-  }
-
-  @BeforeUpdate()
-  async passwordUpdate(event: UpdateEvent<User>) {
-    if (
-      typeof event.entity.password === 'string' &&
-      !!event.entity.password.trim() &&
-      event.entity.password !== event.databaseEntity.password
-    ) {
-      event.entity.password = await argon2.hash(event.entity.password);
-    }
-  }
-
-  async verifyPassword(password: string): Promise<boolean> {
-    return await argon2.verify(this.password, password);
-  }
 
   @Column({ default: true })
   allow_login: boolean;
---
+++ src/users/entities/user.subscriber.ts
@@ -0,0 +1,36 @@
+import * as argon2 from 'argon2';
+import {
+  Connection,
+  EntitySubscriberInterface,
+  EventSubscriber,
+  InsertEvent,
+  UpdateEvent,
+} from 'typeorm';
+import { User } from './user.entity';
+
+@EventSubscriber()
+export class UserSubscriber implements EntitySubscriberInterface<User> {
+  constructor(private connection: Connection) {
+    this.connection.subscribers.push(this);
+  }
+
+  listenTo() {
+    return User;
+  }
+
+  async beforeInsert(event: InsertEvent<User>) {
+    if (event.entity.password) {
+      event.entity.password = await argon2.hash(event.entity.password);
+    }
+  }
+
+  async beforeUpdate(event: UpdateEvent<User>) {
+    if (
+      typeof event.entity.password === 'string' &&
+      !!event.entity.password.trim() &&
+      event.entity.password !== event.databaseEntity.password
+    ) {
+      event.entity.password = await argon2.hash(event.entity.password);
+    }
+  }
+}
