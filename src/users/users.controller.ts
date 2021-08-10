6a50083668448ec5a00b465e8f19b25e1c846840
Mix <32300164+mnixry@users.noreply.github.com>
2021-08-11 01:26:05 +0800

    :sparkles: Add user register endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,8 +5,10 @@ import {
   Post,
   Request,
   UseGuards,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
-import { ApiBearerAuth, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Crud, CrudController } from '@nestjsx/crud';
 import { UserAuthGuard, UserJwtAuthGuard } from './auth/user-auth.guard';
 import { UserAuthService } from './auth/user-auth.service';
@@ -22,24 +24,37 @@ import { UsersService } from './users.service';
   routes: { exclude: ['replaceOneBase'] },
   dto: { create: CreateUserDto, update: UpdateUserDto },
   params: { name: { field: 'name', type: 'string', primary: true } },
-  query: { alwaysPaginate: true, maxLimit: 50 },
+  query: {
+    alwaysPaginate: true,
+    maxLimit: 30,
+    join: { roles: { eager: true } },
+  },
 })
 @ApiTags('users')
 @Controller('users')
 export class UsersController implements CrudController<User> {
   constructor(public service: UsersService, private auth: UserAuthService) {}
 
-  @ApiCreatedResponse({ type: AccessTokenDto })
+  @UsePipes(new ValidationPipe({ transform: true }))
+  @Post('auth/register')
+  async userRegister(@Body() user: CreateUserDto): Promise<User> {
+    return this.service.userRegister(user);
+  }
+
   @UseGuards(UserAuthGuard)
   @Post('auth/login')
-  userLogin(@Request() req: Express.Request, @Body() body: LoginUserDto) {
-    return this.auth.certificateUser(req.user as User);
+  userLogin(
+    @Request() req: Express.Request,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    @Body() body: LoginUserDto,
+  ): AccessTokenDto {
+    return this.auth.certificateUser(req.user);
   }
 
   @ApiBearerAuth()
   @UseGuards(UserJwtAuthGuard)
   @Get('profile')
-  getProfile(@Request() req: Express.Request) {
-    return req.user as User;
+  getProfile(@Request() req: Express.Request): User {
+    return req.user;
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
+import { InjectConnection, InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
-import { Repository } from 'typeorm';
+import { AppRoles } from 'src/app.roles';
+import { Connection, Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserRoles } from './entities/user-roles.entity';
 import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService extends TypeOrmCrudService<User> {
-  constructor(@InjectRepository(User) repo: Repository<User>) {
+  constructor(
+    @InjectRepository(User) repo: Repository<User>,
+    @InjectConnection() private readonly connection: Connection,
+  ) {
     super(repo);
+  }
+
+  async userRegister(user: CreateUserDto) {
+    const { id } = await this.connection.transaction(async (manager) => {
+      const userEntity = manager.merge(User, new User(), user);
+      await manager.save(userEntity);
+      const roleEntity = manager.merge(UserRoles, new UserRoles(), {
+        user: userEntity,
+        role: AppRoles.REGISTERED_USER,
+      } as UserRoles);
+      await manager.save(roleEntity);
+      return userEntity;
+    });
+    return this.repo.findOne(id);
   }
 }
