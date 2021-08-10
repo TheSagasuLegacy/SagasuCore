d234226aa26e02f3fa912f8cdae547e416e4fbc7
Mix <32300164+mnixry@users.noreply.github.com>
2021-08-11 01:25:35 +0800

    :bug: Fix wrong regular expression caused validation error

--- src/users/users.constants.ts
+++ src/users/users.constants.ts
@@ -6,6 +6,6 @@
     Must not contain a colon (:); an ampersand (&); a period (.); a tilde (~); or a space.
  * Source: https://stackoverflow.com/questions/16965953/regular-expressions-for-username-and-password
  */
-export const USER_PASS_REGEX = /^(?=[^a-z]*[a-z])(?=\D*\d)[^:&.~\s]{5,20}$/gm;
+export const USER_PASS_REGEX = /^(?=[^a-z]*[a-z])(?=\D*\d)[^:&.~\s]{5,20}$/;
 
-export const USER_NAME_REGEX = /^[a-z][a-z0-9-_]{3,32}$/gm;
+export const USER_NAME_REGEX = /^[a-z][a-z0-9-_]{3,32}$/;
