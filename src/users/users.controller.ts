d2181eadd4b585e53afe29e3a2838714843f7a19
Mix <32300164+mnixry@users.noreply.github.com>
2021-08-14 01:23:31 +0800

    :recycle: Refactor users controller with custom made crud base class

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,22 +1,25 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
+  Param,
+  Patch,
   Post,
+  Query,
   Request,
   UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { Crud, CrudController } from '@nestjsx/crud';
+import { CreateManyDto, PaginationDto } from 'src/crud.service';
 import {
   AccessControlGuard,
   BypassJwtAuthGuard,
   UserAuthGuard,
   UserJwtAuthGuard,
 } from './auth/user-auth.guard';
-import { UserAuthInterceptor } from './auth/user-auth.interceptor';
 import { UserAuthService } from './auth/user-auth.service';
 import { AccessTokenDto } from './dto/access-token.dto';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -25,46 +28,59 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
-@Crud({
-  model: { type: User },
-  routes: {
-    exclude: ['replaceOneBase'],
-    getOneBase: {
-      decorators: [ApiBearerAuth()],
-      interceptors: [UserAuthInterceptor],
-    },
-  },
-  dto: { create: CreateUserDto, update: UpdateUserDto },
-  params: { name: { field: 'name', type: 'string', primary: true } },
-  query: {
-    alwaysPaginate: true,
-    maxLimit: 30,
-    join: { roles: { eager: true } },
-    filter: { allow_login: { $ne: false } },
-  },
-})
 @ApiTags('users')
 @Controller('users')
 @UseGuards(UserJwtAuthGuard, AccessControlGuard)
-export class UsersController implements CrudController<User> {
+@UsePipes(new ValidationPipe({ transform: true }))
+export class UsersController {
   constructor(
     public service: UsersService,
     private readonly auth: UserAuthService,
   ) {}
 
-  get base(): CrudController<User> {
-    return this;
+  @ApiBearerAuth()
+  @Get('/')
+  getMany(@Query() options: PaginationDto) {
+    return this.service.getMany(options);
+  }
+
+  @ApiBearerAuth()
+  @Get('/:name')
+  getOne(@Param('name') name: string) {
+    return this.service.getOne(name);
+  }
+
+  @ApiBearerAuth()
+  @Post('/')
+  createOne(@Body() dto: CreateUserDto) {
+    return this.service.createOne(dto);
+  }
+
+  @ApiBearerAuth()
+  @Post('/bulk')
+  createMany(@Body() dto: CreateManyDto<CreateUserDto>) {
+    return this.service.createMany(dto);
+  }
+
+  @ApiBearerAuth()
+  @Patch('/:name')
+  updateOne(@Param('name') name: string, @Body() dto: UpdateUserDto) {
+    return this.service.updateOne(name, dto);
+  }
+
+  @ApiBearerAuth()
+  @Delete('/:name')
+  deleteOne(@Param('name') name: string) {
+    return this.service.deleteOne(name);
   }
 
   @BypassJwtAuthGuard()
-  @UsePipes(new ValidationPipe({ transform: true }))
   @Post('auth/register')
   async userRegister(@Body() user: CreateUserDto): Promise<User> {
     return this.service.userRegister(user);
   }
 
   @BypassJwtAuthGuard()
-  @UsePipes(new ValidationPipe({ transform: true }))
   @UseGuards(UserAuthGuard)
   @Post('auth/login')
   userLogin(
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { InjectConnection, InjectRepository } from '@nestjs/typeorm';
-import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { AppRoles } from 'src/app.roles';
+import { CrudBaseService } from 'src/crud.service';
 import { Connection, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserRoles } from './entities/user-roles.entity';
 import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService extends TypeOrmCrudService<User> {
+export class UsersService extends CrudBaseService<User> {
   constructor(
     @InjectRepository(User) repo: Repository<User>,
     @InjectConnection() private readonly connection: Connection,
   ) {
-    super(repo);
+    super(repo, 'name');
   }
 
   async userRegister(user: CreateUserDto) {
