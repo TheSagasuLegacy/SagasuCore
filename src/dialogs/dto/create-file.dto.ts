a4cbaaef3886f7b86f5ed2f82b67a00db35fee86
Mix <32300164+mnixry@users.noreply.github.com>
2021-08-20 03:51:47 +0800

    :lock: Add filename validation to create file DTO

--- src/dialogs/dto/create-file.dto.ts
+++ src/dialogs/dto/create-file.dto.ts
@@ -1,8 +1,32 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsHash, Length } from 'class-validator';
+import {
+  IsHash,
+  Length,
+  registerDecorator,
+  ValidationOptions,
+} from 'class-validator';
+import * as isFilename from 'valid-filename';
+
+export function IsFilename(validationOptions?: ValidationOptions) {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'isLongerThan',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: unknown) {
+          return typeof value === 'string' && isFilename(value);
+        },
+      },
+    });
+  };
+}
 
 export class CreateSubtitleFileDto {
   @ApiProperty({ minLength: 4, maxLength: 1024 })
+  @IsFilename()
   @Length(4, 1024)
   filename: string;
 
