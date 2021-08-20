42c52e3b0def3fc0383e9922916ab426b76c3861
Mix <32300164+mnixry@users.noreply.github.com>
2021-08-21 02:40:25 +0800

    :recycle: Refactor files controller with custom made crud base class

--- src/dialogs/files.controller.ts
+++ src/dialogs/files.controller.ts
@@ -1,33 +1,98 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
-import { Crud, CrudController } from '@nestjsx/crud';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+  UseInterceptors,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { AppRoles } from 'src/app.roles';
+import {
+  CreateMany,
+  PaginatedResult,
+  PaginationOptions,
+} from 'src/crud-base.models';
+import {
+  AccessControlGuard,
+  HasRoles,
+  UserJwtAuthGuard,
+} from 'src/users/auth/user-auth.guard';
 import { CreateSubtitleFileDto } from './dto/create-file.dto';
 import { UpdateSubtitleFileDto } from './dto/update-file.dto';
 import { SubtitleFile } from './entities/files.entity';
 import { FilesService } from './files.service';
 
-@Crud({
-  model: { type: SubtitleFile },
-  dto: { create: CreateSubtitleFileDto, update: UpdateSubtitleFileDto },
-  routes: { exclude: ['replaceOneBase'] },
-  params: { id: { field: 'id', type: 'uuid', primary: true } },
-  query: {
-    alwaysPaginate: true,
-    join: { series: { eager: true }, episode: { eager: true } },
-    maxLimit: 50,
-  },
-})
+class CreateManyFiles extends CreateMany(CreateSubtitleFileDto) {}
+
+class PaginateFilesResult extends PaginatedResult(SubtitleFile) {}
+
+class PaginateFilesOptions extends PaginationOptions(SubtitleFile) {}
+
 @ApiTags('files')
 @Controller('files')
-export class FilesController implements CrudController<SubtitleFile> {
+@UseInterceptors(ClassSerializerInterceptor)
+@UseGuards(UserJwtAuthGuard, AccessControlGuard)
+@UsePipes(new ValidationPipe({ transform: true }))
+export class FilesController {
   constructor(public service: FilesService) {}
 
-  @Get('sha1/:sha1')
+  @ApiBearerAuth()
+  @HasRoles(AppRoles.READ_MULTIPLE_FILE)
+  @Get('/')
+  getMany(
+    @Query() options: PaginateFilesOptions,
+  ): Promise<PaginateFilesResult> {
+    return this.service.getMany(options);
+  }
+
+  @ApiBearerAuth()
+  @Get('/:id')
+  getOne(@Param('id') id: string) {
+    return this.service.getOne(id);
+  }
+
+  @ApiBearerAuth()
+  @Post('/')
+  createOne(@Body() dto: CreateSubtitleFileDto) {
+    return this.service.createOne(dto);
+  }
+
+  @ApiBearerAuth()
+  @Post('/bulk')
+  createMany(@Body() dto: CreateManyFiles) {
+    return this.service.createMany(dto);
+  }
+
+  @ApiBearerAuth()
+  @Patch('/:id')
+  updateOne(@Param('id') id: string, @Body() dto: UpdateSubtitleFileDto) {
+    return this.service.updateOne(id, dto);
+  }
+
+  @ApiBearerAuth()
+  @Delete('/:id')
+  deleteOne(@Param('id') id: string) {
+    return this.service.deleteOne(id);
+  }
+
+  @ApiBearerAuth()
+  @HasRoles(AppRoles.READ_SINGLE_FILE)
+  @Get('/sha1/:sha1')
   async getBySha1(@Param('sha1') sha1: string) {
     return this.service.getBySha1(sha1);
   }
 
-  @Get('series/:id')
+  @ApiBearerAuth()
+  @HasRoles(AppRoles.READ_MULTIPLE_FILE)
+  @Get('/series/:id')
   async getBySeriesId(@Param('id') id: number) {
     return this.service.getBySeriesId(id);
   }
--- src/dialogs/files.service.ts
+++ src/dialogs/files.service.ts
@@ -1,13 +1,65 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { AppResources } from 'src/app.roles';
+import { CrudBaseService } from 'src/crud-base.service';
 import { Repository } from 'typeorm';
+import { CreateSubtitleFileDto } from './dto/create-file.dto';
+import { UpdateSubtitleFileDto } from './dto/update-file.dto';
 import { SubtitleFile } from './entities/files.entity';
 
 @Injectable()
-export class FilesService extends TypeOrmCrudService<SubtitleFile> {
+export class FilesService extends CrudBaseService<
+  SubtitleFile,
+  string,
+  CreateSubtitleFileDto,
+  UpdateSubtitleFileDto
+> {
   constructor(@InjectRepository(SubtitleFile) repo: Repository<SubtitleFile>) {
-    super(repo);
+    super(repo, 'id');
+  }
+
+  canCreate(data: { dto: CreateSubtitleFileDto; user: Express.User }) {
+    return data.dto.user_id === data.user.id
+      ? this.rolesBuilder.can(data.user.roles).createOwn(AppResources.FILE)
+          .granted
+      : this.rolesBuilder.can(data.user.roles).createAny(AppResources.FILE)
+          .granted;
+  }
+
+  canRead(data: {
+    primary?: string;
+    entity?: SubtitleFile;
+    user: Express.User;
+  }) {
+    return data.entity?.user_id === data.user.id
+      ? this.rolesBuilder.can(data.user.roles).readOwn(AppResources.FILE)
+          .granted
+      : this.rolesBuilder.can(data.user.roles).readAny(AppResources.FILE)
+          .granted;
+  }
+
+  canUpdate(data: {
+    dto: UpdateSubtitleFileDto;
+    entity: SubtitleFile;
+    user: Express.User;
+  }) {
+    return data.entity.user_id === data.user.id
+      ? this.rolesBuilder.can(data.user.roles).updateOwn(AppResources.FILE)
+          .granted
+      : this.rolesBuilder.can(data.user.roles).updateAny(AppResources.FILE)
+          .granted;
+  }
+
+  canDelete(data: {
+    primary?: string;
+    entity?: SubtitleFile;
+    user: Express.User;
+  }) {
+    return data.entity?.user_id === data.user.id
+      ? this.rolesBuilder.can(data.user.roles).deleteOwn(AppResources.FILE)
+          .granted
+      : this.rolesBuilder.can(data.user.roles).deleteAny(AppResources.FILE)
+          .granted;
   }
 
   async getBySha1(sha1: string): Promise<SubtitleFile> {
