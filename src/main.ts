98214923652777af531468e3afc456326857cebc
Mix <32300164+mnixry@users.noreply.github.com>
2021-08-25 01:56:01 +0800

    :art: Ensure `newrelic` is loaded first

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
+require('newrelic');
+
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as compression from 'compression';
 import * as express from 'express';
-import 'newrelic';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
